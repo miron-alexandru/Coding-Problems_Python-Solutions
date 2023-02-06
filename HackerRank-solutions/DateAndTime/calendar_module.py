5b34df6baf30943595954dccb50b465d75e93a2e
Ake <54357112+KhadaAke@users.noreply.github.com>
2023-02-06 14:40:05 +0200

    Create calendar_module.py

---
+++ HackerRank-solutions/DateAndTime/calendar_module.py
@@ -0,0 +1,21 @@
+# Problem link: https://www.hackerrank.com/challenges/calendar-module/problem
+
+
+
+# My solution:
+# Without calendar module.
+
+import datetime
+
+m, d, y = map(int, input().split())
+answer = datetime.date(y, m, d)
+print(answer.strftime("%A").upper())
+
+
+# With calendar module
+import datetime
+import calendar
+
+m, d, y = map(int, input().split())
+answer = datetime.date(y, m, d)
+print(calendar.day_name[answer.weekday()].upper())
