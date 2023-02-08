d557c8673b5411842fcaeb1839b40dd59a53937a
Ake <54357112+KhadaAke@users.noreply.github.com>
2023-02-08 10:44:20 +0200

    Add files via upload

---
+++ Exercises/PYnative/OOP/check_type.py
@@ -0,0 +1,13 @@
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+class Bus(Vehicle):
+    pass
+
+School_bus = Bus("School Volvo", 12, 50)
+
+# Python's built-in type()
+print(type(School_bus))
