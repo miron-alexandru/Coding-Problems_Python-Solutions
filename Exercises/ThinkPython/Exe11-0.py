39c831b126b8b4a4476512430338c0b6beb56103
Ake <54357112+KhadaAke@users.noreply.github.com>
2023-02-08 10:53:57 +0200

    Add files via upload

---
+++ Exercises/ThinkPython/Exe11-0.py
@@ -0,0 +1,25 @@
+def histogram(s):
+	d = dict()
+	for c in s:
+		if c not in d:
+			d[c] = 1
+		else:
+			d[c] += 1
+	return d
+
+h = histogram('brontosaurus')
+print(h)
+
+def histogram2(s):
+	d = dict()
+	for c in s:
+		d[c] = 1 + d.get(c, 0)
+
+	return d
+print()
+
+
+
+
+h = histogram2('brontosaurus')
+print(h)
---
+++ Exercises/ThinkPython/Exe9-5.py
@@ -0,0 +1,17 @@
+def uses_all(word, letters):
+	required = letters.split()
+	for letter in required:
+		if letter not in word:
+			return False
+	return True
+
+
+file = open('words.txt')
+count = 0
+
+for line in file:
+	word = line.strip()
+	if uses_all(word, 'a e i o u y') == True:
+		print(word)
+		count += 1
+print(count)
