180e7d7502c7d9aa23054b9d808c4c184409389b
Ake <54357112+KhadaAke@users.noreply.github.com>
2023-02-13 18:29:53 +0200

    Update alien.py

--- My-AlienInvasion-Game/alien.py
+++ My-AlienInvasion-Game/alien.py
@@ -4,20 +4,17 @@ from pygame.sprite import Sprite
 
 
 class Alien(Sprite):
-	"""A class to represent a single alien in the fleet."""
-	def __init__(self,ai_game):
+	"""A class to represent an alien."""
+	def __init__(self, ai_game):
 		"""Initialize the alien and set its starting position."""
 		super().__init__()
 		self.screen = ai_game.screen
 		self.settings = ai_game.settings
-		#Load the alien image and set its rect attribute.
-		self.image = pygame.image.load('images/alien.png')
+		self.image = pygame.image.load('images/alien1.png')
 		self.rect = self.image.get_rect()
-		#Start each new alien near the top left of the screen.
-		self.rect.x = self.rect.width
+		self.rect.x = self.rect.width   # Start each new alien near the top left of the screen.
 		self.rect.y = self.rect.height
-		#Store the alien's exact horizontal position.
-		self.x = float(self.rect.x)
+		self.x = float(self.rect.x)   # Store the alien's exact horizontal position.
 
 	def check_edges(self):
 		"""Return True if alien is at edge of screen."""
@@ -30,7 +27,3 @@ class Alien(Sprite):
 		self.x += (self.settings.alien_speed *
 					self.settings.fleet_direction)
 		self.rect.x = self.x
-
-
-
-
