95765657bf5c374cbd1041eb5dc3459e4b44a7a2
Ake <54357112+KhadaAke@users.noreply.github.com>
2023-02-13 19:37:32 +0200

    Update ships_aliens_img.py

--- My-AlienInvasion-Game/ships_aliens_img.py
+++ My-AlienInvasion-Game/ships_aliens_img.py
@@ -1,15 +1,22 @@
 import pygame
 
-img1 = pygame.image.load('images/ship.png')
-img2 = pygame.image.load('images/ship2.bmp')
-img3 = pygame.image.load('images/ship3.png')
-img4 = pygame.image.load('images/ship4.png')
-img5 = pygame.image.load('images/ship5.png')
-img6 = pygame.image.load('images/ship6.png')
-images = [img1, img2, img3, img4, img5, img6]
 
-alimg1 = pygame.image.load('images/alien.png')
-alimg2 = pygame.image.load('images/alien2.png')
-alimg3 = pygame.image.load('images/alien3.png')
-alimg4 = pygame.image.load('images/alien4.png')
-aliens_imgs = [alimg1, alimg2, alimg3, alimg4]
+
+# Ship images
+ship_image_one = pygame.image.load('images/ship.png')
+ship_image_two = pygame.image.load('images/ship2.bmp')
+ship_image_three = pygame.image.load('images/ship3.png')
+ship_image_four = pygame.image.load('images/ship4.png')
+ship_image_five = pygame.image.load('images/ship5.png')
+ship_image_six = pygame.image.load('images/ship6.png')
+ship_images = [ship_image_one, ship_image_two, ship_image_three, ship_image_four, ship_image_five, ship_image_six]
+
+
+# Alien images
+alien_img_one = pygame.image.load('images/alien1.png')
+alien_img_two = pygame.image.load('images/alien2.png')
+alien_img_three = pygame.image.load('images/alien3.png')
+alien_img_four = pygame.image.load('images/alien4.png')
+aliens_img = [alien_img_one, alien_img_two, alien_img_three, alien_img_four]
+
+
