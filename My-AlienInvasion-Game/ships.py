4c67750443d7e31be7501e5aefdbc61936f0371c
Ake <54357112+KhadaAke@users.noreply.github.com>
2023-02-13 19:37:04 +0200

    Update ships.py

--- My-AlienInvasion-Game/ships.py
+++ My-AlienInvasion-Game/ships.py
@@ -1,8 +1,10 @@
 import pygame
+import random
 from pygame.sprite import Sprite
 
-class Ship(Sprite):
-	"""A class to manage the ship."""
+
+class Thunderbird(Sprite):
+	"""A class to manage the Player 1 ship."""
 
 	def __init__(self,ai_game,x, y):
 		"""Initialize the ship and set its starting position."""
@@ -10,20 +12,14 @@ class Ship(Sprite):
 		self.screen = ai_game.screen
 		self.settings = ai_game.settings
 		self.screen_rect = ai_game.screen.get_rect()
-
-		#Load the ship image and get its rect.
 		self.image = pygame.image.load('images/ship.png')
 		self.rect = self.image.get_rect(topleft = (227, 592))
 
-
-		#Start each new ship at the bottom center of the screen.
-		#self.rect.midbottom = self.screen_rect.midbottom
-
-		#Store a decimal value for the ship's horizontal position.
+		# Store a decimal value for the ship's horizontal position.
 		self.x = float(self.rect.x)
 		self.y = float(self.rect.y)
 
-		#Movement flags
+		# Movement flags
 		self.moving_right = False
 		self.moving_left = False
 		self.moving_up = False
@@ -31,18 +27,18 @@ class Ship(Sprite):
 
 	def update(self):
 		"""Update the ship's position based on the movement flags."""
-		#Update the ship's x value, not the rect.
+		# Update the ship's x value, not the rect.
 		if self.moving_right and self.rect.right < self.screen_rect.right:
-			self.x += self.settings.ship_speed
+			self.x += self.settings.first_player_ship_speed
 		if self.moving_left and self.rect.left > 0:
-			self.x -= self.settings.ship_speed
+			self.x -= self.settings.first_player_ship_speed
 		if self.moving_up and self.rect.top > 0:
-			self.y -= self.settings.ship_speed
+			self.y -= self.settings.first_player_ship_speed
 		if self.moving_down and self.rect.bottom <= self.screen_rect.bottom:
-			self.y += self.settings.ship_speed
+			self.y += self.settings.first_player_ship_speed
 
 
-		#Update rect object from self.x.
+		# Update rect object from self.x.
 		self.rect.x = self.x - 250
 		self.rect.y = self.y
 			
@@ -57,29 +53,49 @@ class Ship(Sprite):
 		self.x = float(self.rect.x)
 		self.y = float(self.rect.y)	
 
+	def power_up(self):
+		power_up_choice = random.randint(1, 4)
+		if power_up_choice == 1:
+			self.settings.first_player_bullet_speed += 0.5
+		elif power_up_choice == 2:
+			self.settings.first_player_bullet_width += 2
+		elif power_up_choice == 3:
+			self.settings.first_player_ship_speed += 0.5
 
 
 
-class SecondShip(Ship):
+
+class Phoenix(Thunderbird):
+	"""A class to manage the Player 2 ship."""
 	def __init__(self, ai_game,x, y):
 		super().__init__(ai_game, x, y)
+		self.settings = ai_game.settings
 		self.moving_right = False
 		self.moving_left = False
-		self.moving_upp = False
-		self.moving_downn = False
+		self.moving_up = False
+		self.moving_down = False
 		self.image = pygame.image.load('images/ship4.png')
 		self.rect = self.image.get_rect(topleft = (727, 592))
 
 	def update(self):
 		if self.moving_right and self.rect.right < self.screen_rect.right:
-			self.x += self.settings.ship_speed
+			self.x += self.settings.second_player_ship_speed
 		if self.moving_left and self.rect.left > 0:
-			self.x -= self.settings.ship_speed
-		if self.moving_upp and self.rect.top > 0:
-			self.y -= self.settings.ship_speed
-		if self.moving_downn and self.rect.bottom <= self.screen_rect.bottom:
-			self.y += self.settings.ship_speed
+			self.x -= self.settings.second_player_ship_speed
+		if self.moving_up and self.rect.top > 0:
+			self.y -= self.settings.second_player_ship_speed
+		if self.moving_down and self.rect.bottom <= self.screen_rect.bottom:
+			self.y += self.settings.second_player_ship_speed
 
-		#Update rect object from self.x.
+		# Update rect object from self.x.
 		self.rect.x = self.x + 250
 		self.rect.y = self.y
+
+	def power_up(self):
+		power_up_choice = random.randint(1, 4)
+		if power_up_choice == 1:
+			self.settings.second_player_bullet_speed += 0.5
+		elif power_up_choice == 2:
+			self.settings.second_player_bullet_width += 2
+		elif power_up_choice == 3:
+			self.settings.second_player_ship_speed += 0.5
