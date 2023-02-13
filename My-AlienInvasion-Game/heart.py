9ce00f48da438b3e76ba8d3ccf991e5a96f8b43f
Ake <54357112+KhadaAke@users.noreply.github.com>
2023-02-13 19:35:45 +0200

    Update heart.py

--- My-AlienInvasion-Game/heart.py
+++ My-AlienInvasion-Game/heart.py
@@ -1,9 +1,17 @@
 import pygame
+from pygame.sprite import Sprite
 
-from ships import Ship
 
-class Heart(Ship):
+class Heart(Sprite):
+	"""A class that manages the remaining health of player/players"""
 	def __init__(self, ai_game, x, y):
-		Ship.__init__(self, ai_game, x, y)
+		super().__init__()
+		self.screen = ai_game.screen
+		self.settings = ai_game.settings
+		self.screen_rect = ai_game.screen.get_rect()
+
 		self.image = pygame.image.load('images/heart.png')
 		self.rect = self.image.get_rect(topleft = (0, 0))
+
+	def blitme(self):
+		self.screen.blit(self.image, self.rect)
