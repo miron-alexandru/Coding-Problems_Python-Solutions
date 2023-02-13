b48abc942feccd6cc7371e3b3e5dc5a9d6cc388c
Ake <54357112+KhadaAke@users.noreply.github.com>
2023-02-13 19:35:19 +0200

    Update game_stats.py

--- My-AlienInvasion-Game/game_stats.py
+++ My-AlienInvasion-Game/game_stats.py
@@ -1,14 +1,10 @@
 class GameStats:
-	"""Track statistics for Alien Invasion."""
+	"""Track statistics for the game."""
 	def __init__(self, ai_game):
 		"""Initialize statistics."""
 		self.settings = ai_game.settings
 		self.reset_stats()
-
-		#Start Alien Invasion in an inactive state.
-		self.game_active = False	
-
-		#High score should never be reset.
+		self.game_active = False   # Start the game in an inactive state.	
 		self.high_score = 0
 
 	def reset_stats(self):
