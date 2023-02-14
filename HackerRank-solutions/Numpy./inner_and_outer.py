d96ceeceb5e45c3d3a4e0f1c6d040c0554cb897e
Ake <54357112+KhadaAke@users.noreply.github.com>
2023-02-14 13:34:06 +0200

    Add files via upload

---
+++ HackerRank-solutions/Numpy./inner_and_outer.py
@@ -0,0 +1,12 @@
+# Problem Link: https://www.hackerrank.com/challenges/np-inner-and-outer/problem
+
+
+# My Solution:
+import numpy as np
+
+a = np.fromstring(input(), dtype=int, sep=' ')
+b = np.fromstring(input(), dtype=int, sep=' ')
+
+
+print(np.inner(a, b))
+print(np.outer(a,b))
