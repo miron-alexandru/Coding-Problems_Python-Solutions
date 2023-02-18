3af37d55834edde4f5e537c7c95a01b312b656e0
Ake <54357112+KhadaAke@users.noreply.github.com>
2023-02-18 18:16:11 +0200

    Add files via upload

---
+++ Codewars/5kyu/count_ip_address.py
@@ -0,0 +1,10 @@
+# Problem Link:  https://www.codewars.com/kata/526989a41034285187000de4
+
+
+# My Solution:
+import ipaddress
+
+def ips_between(start, end):
+    start_ip = int(ipaddress.IPv4Address(start))
+    end_ip = int(ipaddress.IPv4Address(end))
+    return end_ip - start_ip
