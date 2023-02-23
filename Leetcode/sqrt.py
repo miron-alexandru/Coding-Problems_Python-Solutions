03dfb6f64b502e043922285485ab52d4bceabb16
Ake <54357112+KhadaAke@users.noreply.github.com>
2023-02-23 19:06:12 +0200

    Create sqrt.py

---
+++ Leetcode/sqrt.py
@@ -0,0 +1,9 @@
+# Problem Link: https://leetcode.com/problems/sqrtx/
+
+# My Solution:
+import math
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return math.floor(math.sqrt(x))
+        
