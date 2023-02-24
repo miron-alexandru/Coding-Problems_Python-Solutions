59d04cc570db99e446c94308459531dcb883b347
Ake <54357112+KhadaAke@users.noreply.github.com>
2023-02-24 11:45:58 +0200

    Create Day22_Binary_Search_Trees.py

---
+++ HackerRank-solutions/30DaysOfCode/Day22_Binary_Search_Trees.py
@@ -0,0 +1,11 @@
+# Problem Link: https://www.hackerrank.com/challenges/30-binary-search-trees/problem
+
+# My Solution:
+def getHeight(self,root):
+    if root is None:
+        return -1
+
+    left_height = self.getHeight(root.left)
+    right_height = self.getHeight(root.right)
+
+    return 1 + max(left_height, right_height)
