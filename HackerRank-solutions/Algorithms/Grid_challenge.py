31a4075877acce7ce8e8bfaa385992ed1aea47f5
Ake <54357112+KhadaAke@users.noreply.github.com>
2023-02-24 12:22:34 +0200

    Add files via upload

---
+++ HackerRank-solutions/Algorithms/Grid_challenge.py
@@ -0,0 +1,15 @@
+# Problem Link: https://www.hackerrank.com/challenges/grid-challenge/problem
+
+
+# My Solution:
+def gridChallenge(grid):
+	# Sort each row
+    for i in range(len(grid)):
+        grid[i] = sorted(grid[i])
+    
+    # Check each row for ascending alphabetical order
+    for j in range(len(grid[0])):
+        for i in range(1, len(grid)):
+            if grid[i][j] < grid[i-1][j]:
+                return "NO"
+    return "YES"
