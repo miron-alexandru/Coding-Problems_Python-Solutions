ec5674fba080efe5459e35fe7d8271733fe590be
Ake <54357112+KhadaAke@users.noreply.github.com>
2023-02-27 20:42:31 +0200

    Create time_delta.py

---
+++ HackerRank-solutions/DateAndTime/time_delta.py
@@ -0,0 +1,21 @@
+# Problem Link: https://www.hackerrank.com/challenges/python-time-delta/problem
+
+
+# My Solution: 
+from datetime import datetime
+
+def time_delta(t1, t2):
+    fmt = '%a %d %b %Y %H:%M:%S %z'
+    dt1 = datetime.strptime(t1, fmt)
+    dt2 = datetime.strptime(t2, fmt)
+    delta = abs(int((dt1 - dt2).total_seconds()))
+    return delta
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for t_itr in range(t):
+        t1 = input()
+        t2 = input()
+        delta = time_delta(t1, t2)
+        print(delta)
