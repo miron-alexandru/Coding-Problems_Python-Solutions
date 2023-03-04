d4bebe486b1c17717f1af70cbf9797c1a254add8
Ake <54357112+KhadaAke@users.noreply.github.com>
2023-03-04 13:07:48 +0200

    Add files via upload

---
+++ Leetcode/TopInterviewQuestions/Easy/power_of_three.py
@@ -0,0 +1,17 @@
+# Problem Link: https://leetcode.com/problems/power-of-three/
+
+
+# My Solution:
+import math
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        # use recurion
+        if n == 0:
+            return False
+        if n == 1:
+            return True
+        if n % 3 != 0:
+            return False
+            
+        return self.isPowerOfThree(n // 3)
