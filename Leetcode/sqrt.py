6b80fdedd38ba5949c53b042e9a11db95582951e
Ake <54357112+KhadaAke@users.noreply.github.com>
2023-03-04 12:54:21 +0200

    Delete sqrt.py

--- Leetcode/sqrt.py
+++
@@ -1,9 +0,0 @@
-# Problem Link: https://leetcode.com/problems/sqrtx/
-
-# My Solution:
-import math
-
-class Solution:
-    def mySqrt(self, x: int) -> int:
-        return math.floor(math.sqrt(x))
-        
