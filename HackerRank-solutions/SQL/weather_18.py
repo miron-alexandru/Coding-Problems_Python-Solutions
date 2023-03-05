129e8c0ff30384f3b3cd45a7a0e42c2934b4fe96
Ake <54357112+KhadaAke@users.noreply.github.com>
2023-03-05 17:52:55 +0200

    Update weather_18.py

--- HackerRank-solutions/SQL/weather_18.py
+++ HackerRank-solutions/SQL/weather_18.py
@@ -1,5 +1,9 @@
 # Problem Link: https://www.hackerrank.com/challenges/weather-observation-station-18/problem
 
 # My Solution:
+# forumula
+| x1 - x2 | + | y1 - y2 |
+
+# query
 SELECT ROUND(ABS(MIN(lat_n) - MAX(lat_n)) + ABS(MIN(long_w) - MAX(long_w)), 4) AS distance
 FROM station;
