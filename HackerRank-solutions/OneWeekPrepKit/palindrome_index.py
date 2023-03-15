5ed920017f1d04fee46e2939d6d9392b57465874
Ake <54357112+KhadaAke@users.noreply.github.com>
2023-03-15 09:18:31 +0200

    Create palindrome_index.py

---
+++ HackerRank-solutions/OneWeekPrepKit/palindrome_index.py
@@ -0,0 +1,17 @@
+# Problem Link: https://www.hackerrank.com/challenges/palindrome-index/problem
+
+
+# My Solution:
+def palindromeIndex(s):
+    n = len(s)
+    # check only the first half of the string
+    for i in range(n // 2):
+        if s[i] != s[n - i - 1]:
+            # check if removing the character at position i or position n-i-1 makes the rest of the string a palindrome
+            if s[i+1:n-i] == s[i+1:n-i][::-1]:
+                return i
+            elif s[i:n-i-1] == s[i:n-i-1][::-1]:
+                return n - i - 1
+            else:
+                return -1 
+    return -1
