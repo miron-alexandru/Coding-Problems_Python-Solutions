86e2ea9d96be0d4a98980a4cdcd69ce41967078b
Ake <54357112+KhadaAke@users.noreply.github.com>
2023-03-25 13:47:23 +0200

    Delete HackerRank-solutions/Numpy. directory

--- HackerRank-solutions/Numpy./shape_reshape.py
+++
@@ -1,9 +0,0 @@
-# Problem Link: https://www.hackerrank.com/challenges/np-shape-reshape/problem
-
-
-# My solution: 
-import numpy as np
-
-nums = list(map(int, input().split()))
-arr = np.array(nums)
-print(np.reshape(arr, (3, 3)))
