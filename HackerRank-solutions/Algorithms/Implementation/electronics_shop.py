2c2fe8ddf73daf5209b00ac043235b4756afd549
Ake <khadaake@gmail.com>
2023-03-31 20:53:58 +0300

    practice

---
+++ HackerRank-solutions/Algorithms/Implementation/electronics_shop.py
@@ -0,0 +1,11 @@
+# problem link: https://www.hackerrank.com/challenges/electronics-shop/problem
+
+# my solution::
+def getMoneySpent(keyboards, drives, b):
+    max_cost = -1
+    for k in keyboards:
+        for d in drives:
+            cost = k + d
+            if cost <= b and cost > max_cost:
+                max_cost = cost
+    return max_cost
