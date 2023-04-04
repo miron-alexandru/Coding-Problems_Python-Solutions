516aa36c57547aef6f072de5985e7bd7b234d6b7
Ake <khadaake@gmail.com>
2023-04-04 16:01:13 +0300

    practice

---
+++ HackerRank-solutions/RegexAndParsing/validating_phone_number.py
@@ -0,0 +1,13 @@
+# problem link: https://www.hackerrank.com/challenges/validating-the-phone-number/problem
+
+# my solution:
+import re
+
+n = int(input())
+
+for i in range(n):
+    number = input().strip()
+    if re.match(r'[789]\d{9}$', number):
+        print("YES")
+    else:
+        print("NO")
