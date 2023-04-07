b0e3a3009285467adc363eaf169a135449ae2041
Ake <54357112+KhadaAke@users.noreply.github.com>
2023-04-07 07:01:05 +0300

    Add files via upload

---
+++ HackerRank-solutions/Algorithms/Implementation/extra_long_factorials.py
@@ -0,0 +1,8 @@
+# problem link: https://www.hackerrank.com/challenges/extra-long-factorials/problem
+
+
+# my solution:
+import math
+
+def extraLongFactorials(n):
+    print(math.factorial(n))
---
+++ HackerRank-solutions/Algorithms/Implementation/find_digits.py
@@ -0,0 +1,16 @@
+# problem link: https://www.hackerrank.com/challenges/find-digits/problem
+
+# my solution:
+def findDigits(n):
+    # convert int to str
+    n_str = str(n)
+    count = 0
+
+    for digit_str in n_str:
+        digit = int(digit_str)
+        
+        # check if digit is not 0 and is a divisor of n
+        if digit != 0 and n % digit == 0:
+            count += 1
+            
+    return count
