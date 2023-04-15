ba1fe595bf3b862f55f993aee2183db2f3408207
Ake <khadaake@gmail.com>
2023-04-15 17:00:30 +0300

    practice

---
+++ Leetcode/DataStructure/Day1_Array/contains_duplicate.py
@@ -0,0 +1,11 @@
+# problem link: https://leetcode.com/problems/contains-duplicate
+
+
+# my solution:
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()   # sort the input array in-place
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i+1]:
+                return True
+        return False
