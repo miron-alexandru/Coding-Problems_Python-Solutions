177354ccdf0617bdb12b24446e13b6d2863b89ff
Ake <khadaake@gmail.com>
2023-04-27 16:54:04 +0300

    leetcode practice

---
+++ Leetcode/LeetCode75/Level1/Day10/fibbonaci_number.py
@@ -0,0 +1,23 @@
+# problem link: https://leetcode.com/problems/fibonacci-number
+
+# my solution:
+class Solution:
+    def fib(self, n: int) -> int:
+        # Base case
+        if n <= 1:
+            return n
+        
+        # Initialize two variables a and b to 0 and 1
+        a, b = 0, 1
+        
+        # Loop n-1 times to calculate F(n)
+        for i in range(2, n + 1):
+            # Calculate the sum of a and b
+            c = a + b
+            # Update a to be b
+            a = b
+            # Update b to be c
+            b = c
+        
+        # Return the value of b, which is F(n)
+        return b
---
+++ Leetcode/LeetCode75/Level1/Day11/unique_paths.py
@@ -0,0 +1,29 @@
+# problem link: https://leetcode.com/problems/unique-paths
+
+
+# my solution:
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # Init 2d array up of size m x n
+        up = [[0] * n for i in range(m)]
+        
+        # only one way to reach the starting point
+        up[0][0] = 1
+        
+        # is only one way to reach any point in the first column
+        # going down
+        for i in range(1, m):
+            up[i][0] = 1
+            
+        # only one way to reach any point in the first row
+        # going right
+        for j in range(1, n):
+            up[0][j] = 1
+            
+        # calculate the number of unique paths as the sum of
+        # the number of paths to reach the cell above and the cell to the left
+        for i in range(1, m):
+            for j in range(1, n):
+                up[i][j] = up[i-1][j] + up[i][j-1]
+
+        return up[m-1][n-1]
