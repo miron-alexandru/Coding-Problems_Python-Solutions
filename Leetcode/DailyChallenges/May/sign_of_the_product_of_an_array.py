e01e69531c9a65449dcc636b94bd94ee17a830d5
Ake <khadaake@gmail.com>
2023-05-02 20:45:07 +0300

    leetcode practice

---
+++ Leetcode/DailyChallenges/May/sign_of_the_product_of_an_array.py
@@ -0,0 +1,12 @@
+# problem link: https://leetcode.com/problems/sign-of-the-product-of-an-array/
+
+# my solution:
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        sign = 1
+        for num in nums:
+            if num == 0:
+                return 0
+            elif num < 0:
+                sign *= -1
+        return sign
