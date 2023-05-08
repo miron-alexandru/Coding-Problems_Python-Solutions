010aab619ec4d1838c77612027fcac61a4190988
Ake <khadaake@gmail.com>
2023-05-08 17:34:55 +0300

    leetcode practice

---
+++ Leetcode/DailyChallenges/May/matrix_diagonal_sum.py
@@ -0,0 +1,14 @@
+# problem link: https://leetcode.com/problems/matrix-diagonal-sum
+
+# solution:
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        n = len(mat)
+        total = 0
+        for i in range(n):
+            for j in range(n):
+            	# check if each element belongs to either of the diagonals
+            	# and if it does, add it to the total
+                if i == j or i + j == n - 1:
+                    total += mat[i][j]
+        return total
--- Leetcode/LeetCode75/NewLeetCode75/merge_strings_alternately.py
+++ Leetcode/LeetCode75/NewLeetCode75/merge_strings_alternately.py
@@ -2,18 +2,15 @@
 
 
 # my solution:
-class Solution:
-    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        # Find the maximum number of candies
-        max_candies = max(candies)
-        
-        # Initialize result array
-        n = len(candies)
-        result = [False] * n
-        
-        # Iterate over candies and update result array
-        for i in range(n):
-            if candies[i] + extraCandies >= max_candies:
-                result[i] = True
 
-        return result
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        merged = ""
+        n = min(len(word1), len(word2))
+        
+        for i in range(n):
+            merged += word1[i] + word2[i]
+        
+        merged += word1[n:] + word2[n:]
+        
+        return merged
