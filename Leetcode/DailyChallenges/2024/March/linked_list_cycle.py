1e79a06d76de0e10386f0b575d419bcb4f80e03f
Ake <khadaake@gmail.com>
2024-03-06 09:50:51 +0200

    daily leetcode challenge.

---
+++ Leetcode/DailyChallenges/2024/March/linked_list_cycle.py
@@ -0,0 +1,20 @@
+# problem link: https://leetcode.com/problems/linked-list-cycle/
+
+
+# my solution:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        visited = set()
+        curr = head
+        while curr:
+            if curr in visited:
+                return True
+            visited.add(curr)
+            curr = curr.next
+        return False
