d070cde2d2bf5b773d7b01b4a46dc8d93d80065e
Ake <khadaake@gmail.com>
2024-03-20 09:39:11 +0200

    daily leetcode.

---
+++ Leetcode/DailyChallenges/2024/March/minimum_length_of_string.py
@@ -0,0 +1,18 @@
+# problem link: https://leetcode.com/problems/minimum-length-of-string-after-deleting-similar-ends/
+
+
+# my solution:
+
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        i = 0
+        j = len(s) - 1
+
+        while i < j and s[i] == s[j]:
+            c = s[i]
+            while i <= j and s[i] == c:
+                i += 1
+            while i <= j and s[j] == c:
+                j -= 1
+
+        return j - i + 1
