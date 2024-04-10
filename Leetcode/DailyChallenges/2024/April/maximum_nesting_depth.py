bfc79973443896bb633e88c029b664a5a87b458d
Ake <khadaake@gmail.com>
2024-04-10 10:56:23 +0300

    daily leetcode challenges.'

---
+++ Leetcode/DailyChallenges/2024/April/maximum_nesting_depth.py
@@ -0,0 +1,17 @@
+# problem link: https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
+
+
+# my solution:
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        max_depth = 0
+        current_depth = 0
+
+        for char in s:
+            if char == '(':
+                current_depth += 1
+                max_depth = max(max_depth, current_depth)
+            elif char == ')':
+                current_depth -= 1
+
+        return max_depth
