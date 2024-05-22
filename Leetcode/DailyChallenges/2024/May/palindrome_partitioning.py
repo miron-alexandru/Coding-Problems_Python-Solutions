7890c313c53c3edb34d9f4e4aa356b5e33f35510
Ake <khadaake@gmail.com>
2024-05-22 10:00:03 +0300

    daily leetcode.

---
+++ Leetcode/DailyChallenges/2024/May/palindrome_partitioning.py
@@ -0,0 +1,27 @@
+# problem link: https://leetcode.com/problems/palindrome-partitioning
+
+
+# my solution:
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        # define two funcs, palindrome and backtrack
+        def palindrome(string):
+            return string == string[::-1]
+        
+        def backtrack(start, path):
+            if start == len(s):
+                partitions.append(path[:])
+                return
+            
+            for i in range(start, len(s)):
+                substring = s[start:i+1]
+                if palindrome(substring):
+                    path.append(substring)
+                    backtrack(i+1, path)
+                    path.pop()
+        
+        partitions = []
+        backtrack(0, [])
+        return partitions
+        
