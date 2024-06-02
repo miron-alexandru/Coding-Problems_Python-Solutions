b1768c23ea518c18b1a93fca333dbd405a81851e
Ake <khadaake@gmail.com>
2024-06-02 12:09:49 +0300

    daily leetcode.

---
+++ Leetcode/DailyChallenges/2024/June/reverse_string.py
@@ -0,0 +1,13 @@
+# problem link: https://leetcode.com/problems/reverse-string/
+
+
+
+# my solution:
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        return s.reverse()
+        
