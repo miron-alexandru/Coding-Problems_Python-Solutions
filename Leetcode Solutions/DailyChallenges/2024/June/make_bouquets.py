6d2b2ba75820a6ae8e6fda54e2683dc2600c1f1c
Ake <khadaake@gmail.com>
2024-06-19 06:03:30 +0300

    daily leetcode.

---
+++ Leetcode Solutions/DailyChallenges/2024/June/make_bouquets.py
@@ -0,0 +1,33 @@
+# problem link: https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/
+
+
+# my solution:
+class Solution:
+    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+        if len(bloomDay) < m * k:
+            return -1
+        
+        def canMakeBouquets(days):
+            bouquets = 0
+            flowers = 0
+            for bloom in bloomDay:
+                if bloom <= days:
+                    flowers += 1
+                    if flowers == k:
+                        bouquets += 1
+                        flowers = 0
+                else:
+                    flowers = 0
+                if bouquets >= m:
+                    return True
+            return False
+
+        left, right = min(bloomDay), max(bloomDay)
+        while left < right:
+            mid = (left + right) // 2
+            if canMakeBouquets(mid):
+                right = mid
+            else:
+                left = mid + 1
+        
+        return left if canMakeBouquets(left) else -1
