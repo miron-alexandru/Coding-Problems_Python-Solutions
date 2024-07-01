3aff8bb72c2ff405a3232f151f7c43dceef258f2
Ake <khadaake@gmail.com>
2024-07-01 12:55:19 +0300

    daily leetcode.

---
+++ Leetcode Solutions/DailyChallenges/2024/July/three_consecutive_odds.py
@@ -0,0 +1,10 @@
+# problem link: https://leetcode.com/problems/three-consecutive-odds/
+
+# my solution:
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        for i in range(len(arr) - 2):
+            # Check if three consecutive numbers are odd
+            if arr[i] % 2 != 0 and arr[i+1] % 2 != 0 and arr[i+2] % 2 != 0:
+                return True
+        return False
