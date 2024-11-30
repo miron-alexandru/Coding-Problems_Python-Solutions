b1387135894c59ae1f784e003afdb70ac9a3984a
Ake <khadaake@gmail.com>
2024-11-30 11:35:41 +0200

    daily leetcode problem - counter number of fair pairs.

---
+++ Leetcode Solutions/DailyChallenges/2024/November/count_number_of_fair_pairs.py
@@ -0,0 +1,18 @@
+# problem link: https://leetcode.com/problems/count-the-number-of-fair-pairs/
+
+# my solution:
+
+from bisect import bisect_left, bisect_right
+
+class Solution:
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        nums.sort()
+        count = 0
+        n = len(nums)
+        
+        for i in range(n):
+            left = bisect_left(nums, lower - nums[i], i + 1, n)
+            right = bisect_right(nums, upper - nums[i], i + 1, n)
+            count += (right - left)
+        
+        return count
