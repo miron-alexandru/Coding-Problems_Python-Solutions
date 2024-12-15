76a88629e7247a80e7c0ebed5973ac3b42b00bae
Ake <khadaake@gmail.com>
2024-12-15 11:12:39 +0200

    daily leetcode problem: special array II.

---
+++ Leetcode Solutions/DailyChallenges/2024/December/is_special_array_II.py
@@ -0,0 +1,12 @@
+# problem link: https://leetcode.com/problems/special-array-ii/
+
+
+# solution:
+class Solution:
+    def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
+        n = len(nums)
+        d = list(range(n))
+        for i in range(1, n):
+            if nums[i] % 2 != nums[i - 1] % 2:
+                d[i] = d[i - 1]
+        return [d[t] <= f for f, t in queries]
