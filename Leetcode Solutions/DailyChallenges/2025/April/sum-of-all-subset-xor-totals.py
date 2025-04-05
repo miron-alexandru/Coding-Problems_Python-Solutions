1697ed660fc2233ca742313cb48fa5805340c04a
Ake <khadaake@gmail.com>
2025-04-05 13:49:19 +0300

    daily leetcode problem.

---
+++ Leetcode Solutions/DailyChallenges/2025/April/sum-of-all-subset-xor-totals.py
@@ -0,0 +1,15 @@
+# problem link: https://leetcode.com/problems/sum-of-all-subset-xor-totals/
+
+
+
+# solution:
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        ans, n = 0, len(nums)
+        for i in range(1 << n):
+            s = 0
+            for j in range(n):
+                if i >> j & 1:
+                    s ^= nums[j]
+            ans += s
+        return ans
