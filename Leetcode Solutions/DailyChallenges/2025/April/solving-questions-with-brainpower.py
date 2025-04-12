efbf5dcf132077a09262cce21059df917af1f532
Ake <khadaake@gmail.com>
2025-04-12 14:29:56 +0300

    daily leetcode.

---
+++ Leetcode Solutions/DailyChallenges/2025/April/solving-questions-with-brainpower.py
@@ -0,0 +1,13 @@
+# problem link: https://leetcode.com/problems/solving-questions-with-brainpower/
+
+
+# solution:
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        n = len(questions)
+        f = [0] * (n + 1)
+        for i in range(n - 1, -1, -1):
+            p, b = questions[i]
+            j = i + b + 1
+            f[i] = max(f[i + 1], p + (0 if j > n else f[j]))
+        return f[0]
