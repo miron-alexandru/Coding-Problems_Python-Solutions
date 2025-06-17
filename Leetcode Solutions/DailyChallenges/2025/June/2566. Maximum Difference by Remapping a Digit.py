b5353fa80101fc5740207ae74f390f681c27fbf2
Ake <khadaake@gmail.com>
2025-06-17 19:09:35 +0300

    Daily leetcode problem: 2566. Maximum Difference by Remapping a Digit.

---
+++ Leetcode Solutions/DailyChallenges/2025/June/2566. Maximum Difference by Remapping a Digit.py
@@ -0,0 +1,53 @@
+# problem link: https://leetcode.com/problems/maximum-difference-by-remapping-a-digit/
+
+
+# Description:
+"""
+You are given an integer num. You know that Bob will sneakily remap one of the 10 possible digits (0 to 9) to another digit.
+
+Return the difference between the maximum and minimum values Bob can make by remapping exactly one digit in num.
+
+Notes:
+
+When Bob remaps a digit d1 to another digit d2, Bob replaces all occurrences of d1 in num with d2.
+Bob can remap a digit to itself, in which case num does not change.
+Bob can remap different digits for obtaining minimum and maximum values respectively.
+The resulting number after remapping can contain leading zeroes.
+ 
+
+Example 1:
+
+Input: num = 11891
+Output: 99009
+Explanation: 
+To achieve the maximum value, Bob can remap the digit 1 to the digit 9 to yield 99899.
+To achieve the minimum value, Bob can remap the digit 1 to the digit 0, yielding 890.
+The difference between these two numbers is 99009.
+Example 2:
+
+Input: num = 90
+Output: 99
+Explanation:
+The maximum value that can be returned by the function is 99 (if 0 is replaced by 9) and the minimum value that can be returned by the function is 0 (if 9 is replaced by 0).
+Thus, we return 99.
+ 
+ """
+
+ # Solution:
+ class Solution:
+    def minMaxDifference(self, num: int) -> int:
+        num_str = str(num)
+
+        # replace one digit with '9'
+        max_val = int(num_str)
+        for d in set(num_str):
+            replaced = int(num_str.replace(d, '9'))
+            max_val = max(max_val, replaced)
+
+        # replace one digit with '0'
+        min_val = int(num_str)
+        for d in set(num_str):
+            replaced = int(num_str.replace(d, '0'))
+            min_val = min(min_val, replaced)
+
+        return max_val - min_val
