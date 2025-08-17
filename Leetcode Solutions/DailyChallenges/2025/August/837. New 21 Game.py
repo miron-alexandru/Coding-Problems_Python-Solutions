b036016ea1a62bf59d9939b173b81ac201a1b104
Ake <khadaake@gmail.com>
2025-08-17 09:45:45 +0300

    Solved daily leetcode problems: 2264. Largest 3-Same-Digit Number in String and 837. New 21 Game.

---
+++ Leetcode Solutions/DailyChallenges/2025/August/837. New 21 Game.py
@@ -0,0 +1,47 @@
+# problem link: https://leetcode.com/problems/new-21-game/
+
+
+# Description:
+"""
+Alice plays the following game, loosely based on the card game "21".
+
+Alice starts with 0 points and draws numbers while she has less than k points. During each draw, she gains an integer number of points randomly from the range [1, maxPts], where maxPts is an integer. Each draw is independent and the outcomes have equal probabilities.
+
+Alice stops drawing numbers when she gets k or more points.
+
+Return the probability that Alice has n or fewer points.
+
+Answers within 10-5 of the actual answer are considered accepted.
+
+ 
+
+Example 1:
+
+Input: n = 10, k = 1, maxPts = 10
+Output: 1.00000
+Explanation: Alice gets a single card, then stops.
+Example 2:
+
+Input: n = 6, k = 1, maxPts = 10
+Output: 0.60000
+Explanation: Alice gets a single card, then stops.
+In 6 out of 10 possibilities, she is at or below 6 points.
+Example 3:
+
+Input: n = 21, k = 17, maxPts = 10
+Output: 0.73278
+"""
+
+
+# Solution:
+class Solution:
+    def new21Game(self, maxPoints: int, stopThreshold: int, maxDraw: int) -> float:
+        @cache
+        def probability(currentPoints: int) -> float:
+            if currentPoints >= stopThreshold:
+                return int(currentPoints <= maxPoints)
+            if currentPoints == stopThreshold - 1:
+                return min(maxPoints - stopThreshold + 1, maxDraw) / maxDraw
+            return probability(currentPoints + 1) + (probability(currentPoints + 1) - probability(currentPoints + maxDraw + 1)) / maxDraw
+
+        return probability(0)
