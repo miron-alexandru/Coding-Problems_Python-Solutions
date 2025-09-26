347c7af3206444fab01f81e4774f62517470d1f5
Ake <khadaake@gmail.com>
2025-09-26 17:06:01 +0300

    Solved daily leetcode problem: 611. Valid Triangle Number

---
+++ Leetcode Solutions/DailyChallenges/2025/September/611. Valid Triangle Number.py
@@ -0,0 +1,40 @@
+# problem link: https://leetcode.com/problems/valid-triangle-number/
+
+
+# Description:
+"""
+Given an integer array nums, return the number of triplets chosen from the array that can make triangles if we take them as side lengths of a triangle.
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,3,4]
+Output: 3
+Explanation: Valid combinations are: 
+2,3,4 (using the first 2)
+2,3,4 (using the second 2)
+2,2,3
+Example 2:
+
+Input: nums = [4,2,3,4]
+Output: 4
+"""
+
+
+# Solution:
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        count = 0
+
+        for k in range(n - 1, 1, -1):
+            i, j = 0, k - 1
+            while i < j:
+                if nums[i] + nums[j] > nums[k]:
+                    count += j - i
+                    j -= 1
+                else:
+                    i += 1
+        return count
