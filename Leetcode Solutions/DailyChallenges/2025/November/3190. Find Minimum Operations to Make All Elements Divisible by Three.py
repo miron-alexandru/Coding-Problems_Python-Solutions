6a4b666b170e503b1e456a539ddf14378c2ce721
Ake <khadaake@gmail.com>
2025-11-22 11:44:21 +0200

    Solved daily leetcode problem: 3190. Find Minimum Operations to Make All Elements Divisible by Three.

---
+++ Leetcode Solutions/DailyChallenges/2025/November/3190. Find Minimum Operations to Make All Elements Divisible by Three.py
@@ -0,0 +1,42 @@
+# problem link: https://leetcode.com/problems/find-minimum-operations-to-make-all-elements-divisible-by-three/
+
+
+# Description:
+"""
+You are given an integer array nums. In one operation, you can add or subtract 1 from any element of nums.
+
+Return the minimum number of operations to make all elements of nums divisible by 3.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+
+Output: 3
+
+Explanation:
+
+All array elements can be made divisible by 3 using 3 operations:
+
+Subtract 1 from 1.
+Add 1 to 2.
+Subtract 1 from 4.
+Example 2:
+
+Input: nums = [3,6,9]
+
+Output: 0
+"""
+
+
+# Solution:
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        op = 0
+
+        for num in nums:
+            r = num % 3
+            op += min(r, 3 - r)
+
+        return op
