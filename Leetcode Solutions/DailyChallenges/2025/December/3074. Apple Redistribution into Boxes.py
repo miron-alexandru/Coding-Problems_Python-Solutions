8552301dd35b420c544c1da3028d16450d0bd2f6
Ake <khadaake@gmail.com>
2025-12-24 13:22:32 +0200

    Solved daily leetcode problem: 3074. Apple Redistribution into Boxes.

---
+++ Leetcode Solutions/DailyChallenges/2025/December/3074. Apple Redistribution into Boxes.py
@@ -0,0 +1,50 @@
+# problem link: https://leetcode.com/problems/apple-redistribution-into-boxes/
+
+
+# Description:
+"""
+You are given an array apple of size n and an array capacity of size m.
+
+There are n packs where the ith pack contains apple[i] apples. There are m boxes as well, and the ith box has a capacity of capacity[i] apples.
+
+Return the minimum number of boxes you need to select to redistribute these n packs of apples into boxes.
+
+Note that, apples from the same pack can be distributed into different boxes.
+
+ 
+
+Example 1:
+
+Input: apple = [1,3,2], capacity = [4,3,1,5,2]
+Output: 2
+Explanation: We will use boxes with capacities 4 and 5.
+It is possible to distribute the apples as the total capacity is greater than or equal to the total number of apples.
+Example 2:
+
+Input: apple = [5,5,5], capacity = [2,4,2,7]
+Output: 4
+Explanation: We will need to use all the boxes.
+"""
+
+# Solution:
+class Solution:
+    def minimumBoxes(self, apple: List[int], capacity: List[int]) -> int:
+        # total apples to store
+        total = sum(apple)
+
+        # use biggest boxes first
+        capacity.sort(reverse=True)
+
+        used = 0
+        current = 0
+
+        for cap in capacity:
+            current += cap
+            used += 1
+
+            # enough space reached
+            if current >= total:
+                return used
+
+        # all boxes needed
+        return used
