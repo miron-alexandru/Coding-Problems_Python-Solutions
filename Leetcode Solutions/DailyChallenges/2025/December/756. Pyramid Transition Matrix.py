40df97ca406023a24954e275e921067d87bdb61b
Ake <khadaake@gmail.com>
2025-12-29 14:53:05 +0200

    Solved daily leetcode problem: 756. Pyramid Transition Matrix.

---
+++ Leetcode Solutions/DailyChallenges/2025/December/756. Pyramid Transition Matrix.py
@@ -0,0 +1,69 @@
+# problem link: https://leetcode.com/problems/pyramid-transition-matrix/
+
+
+
+# Description:
+"""
+You are stacking blocks to form a pyramid. Each block has a color, which is represented by a single letter. Each row of blocks contains one less block than the row beneath it and is centered on top.
+
+To make the pyramid aesthetically pleasing, there are only specific triangular patterns that are allowed. A triangular pattern consists of a single block stacked on top of two blocks. The patterns are given as a list of three-letter strings allowed, where the first two characters of a pattern represent the left and right bottom blocks respectively, and the third character is the top block.
+
+For example, "ABC" represents a triangular pattern with a 'C' block stacked on top of an 'A' (left) and 'B' (right) block. Note that this is different from "BAC" where 'B' is on the left bottom and 'A' is on the right bottom.
+You start with a bottom row of blocks bottom, given as a single string, that you must use as the base of the pyramid.
+
+Given bottom and allowed, return true if you can build the pyramid all the way to the top such that every triangular pattern in the pyramid is in allowed, or false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: bottom = "BCD", allowed = ["BCC","CDE","CEA","FFF"]
+Output: true
+Explanation: The allowed triangular patterns are shown on the right.
+Starting from the bottom (level 3), we can build "CE" on level 2 and then build "A" on level 1.
+There are three triangular patterns in the pyramid, which are "BCC", "CDE", and "CEA". All are allowed.
+Example 2:
+
+
+Input: bottom = "AAAA", allowed = ["AAB","AAC","BCD","BBE","DEF"]
+Output: false
+Explanation: The allowed triangular patterns are shown on the right.
+Starting from the bottom (level 4), there are multiple ways to build level 3, but trying all the possibilites, you will get always stuck before building level 1.
+"""
+
+
+# Solution:
+class Solution:
+    def pyramidTransition(self, bottom: str, allowed: List[str]) -> bool:
+        tab = defaultdict(set)
+        for u, v, w in allowed:
+            tab[u, v].add(w)
+
+        def add_neighbor(node):
+            res = ['']
+            for i in range(1, len(node)):
+                eles = tab[(node[i - 1], node[i])]
+                if eles:
+                    res = [a + e for e in eles for a in res]
+                else:
+                    return []
+            return res
+        
+        
+        visited = set()
+
+        def dfs(node):
+            if len(node) == 1:
+                return True
+            if node in visited:
+                return False
+
+            for nxt in add_neighbor(node):
+                if dfs(nxt):
+                    return True
+
+            visited.add(node)
+            return False
+
+        return dfs(bottom)
