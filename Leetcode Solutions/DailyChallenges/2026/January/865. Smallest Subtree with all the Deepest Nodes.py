88d2efa795e66cd8f66e68312a1a8d037b75cf4e
Ake <khadaake@gmail.com>
2026-01-14 16:54:59 +0200

    Solved daily leetcode problem: 865. Smallest Subtree with all the Deepest Nodes.

---
+++ Leetcode Solutions/DailyChallenges/2026/January/865. Smallest Subtree with all the Deepest Nodes.py
@@ -0,0 +1,61 @@
+# problem link: https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/
+
+
+
+# Description:
+"""
+Given the root of a binary tree, the depth of each node is the shortest distance to the root.
+
+Return the smallest subtree such that it contains all the deepest nodes in the original tree.
+
+A node is called the deepest if it has the largest depth possible among any node in the entire tree.
+
+The subtree of a node is a tree consisting of that node, plus the set of all descendants of that node.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4]
+Output: [2,7,4]
+Explanation: We return the node with value 2, colored in yellow in the diagram.
+The nodes coloured in blue are the deepest nodes of the tree.
+Notice that nodes 5, 3 and 2 contain the deepest nodes in the tree but node 2 is the smallest subtree among them, so we return it.
+Example 2:
+
+Input: root = [1]
+Output: [1]
+Explanation: The root is the deepest node in the tree.
+Example 3:
+
+Input: root = [0,1,3,null,2]
+Output: [2]
+Explanation: The deepest node in the tree is 2, the valid subtrees are the subtrees of nodes 2, 1 and 0 but the subtree of node 2 is the smallest.
+"""
+
+
+# Solution:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def dfs(node):
+            if not node:
+                return 0, None
+
+            left_depth, left_subtree = dfs(node.left)
+            right_depth, right_subtree = dfs(node.right)
+
+            if left_depth > right_depth:
+                return left_depth + 1, left_subtree
+            elif right_depth > left_depth:
+                return right_depth + 1, right_subtree
+            else:
+                return left_depth + 1, node
+
+        return dfs(root)[1]
