c12c9416952930af3115e39ca6e417f9189d554c
Ake <khadaake@gmail.com>
2026-01-25 13:06:33 +0200

    Solved daily leetcode problem: 1984. Minimum Difference Between Highest and Lowest of K Scores.

---
+++ Leetcode Solutions/DailyChallenges/2026/January/1984. Minimum Difference Between Highest and Lowest of K Scores.py
@@ -0,0 +1,50 @@
+# problem link:https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores/
+
+
+
+# Description:
+"""
+You are given a 0-indexed integer array nums, where nums[i] represents the score of the ith student. You are also given an integer k.
+
+Pick the scores of any k students from the array so that the difference between the highest and the lowest of the k scores is minimized.
+
+Return the minimum possible difference.
+
+ 
+
+Example 1:
+
+Input: nums = [90], k = 1
+Output: 0
+Explanation: There is one way to pick score(s) of one student:
+- [90]. The difference between the highest and lowest score is 90 - 90 = 0.
+The minimum possible difference is 0.
+Example 2:
+
+Input: nums = [9,4,1,7], k = 2
+Output: 2
+Explanation: There are six ways to pick score(s) of two students:
+- [9,4,1,7]. The difference between the highest and lowest score is 9 - 4 = 5.
+- [9,4,1,7]. The difference between the highest and lowest score is 9 - 1 = 8.
+- [9,4,1,7]. The difference between the highest and lowest score is 9 - 7 = 2.
+- [9,4,1,7]. The difference between the highest and lowest score is 4 - 1 = 3.
+- [9,4,1,7]. The difference between the highest and lowest score is 7 - 4 = 3.
+- [9,4,1,7]. The difference between the highest and lowest score is 7 - 1 = 6.
+The minimum possible difference is 2.
+"""
+
+# Solution:
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        if k <= 1:
+            return 0
+
+        nums.sort()
+        best = float("inf")
+
+        for i in range(len(nums) - k + 1):
+            diff = nums[i + k - 1] - nums[i]
+            if diff < best:
+                best = diff
+
+        return best
