818682e5113737f6a215b91196c3533bce28bb0b
Ake <khadaake@gmail.com>
2026-02-01 15:32:36 +0200

    Solved daily leetcode problem: 3010. Divide an Array Into Subarrays With Minimum Cost I.

---
+++ Leetcode Solutions/DailyChallenges/2026/January/744. Find Smallest Letter Greater Than Target.py
@@ -0,0 +1,33 @@
+# problem link: https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+
+
+# Description:
+"""
+You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+
+Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+
+ 
+
+Example 1:
+
+Input: letters = ["c","f","j"], target = "a"
+Output: "c"
+Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
+Example 2:
+
+Input: letters = ["c","f","j"], target = "c"
+Output: "f"
+Explanation: The smallest character that is lexicographically greater than 'c' in letters is 'f'.
+Example 3:
+
+Input: letters = ["x","x","y","y"], target = "z"
+Output: "x"
+Explanation: There are no characters in letters that is lexicographically greater than 'z' so we return letters[0].
+"""
+
+# Solution:
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        index = bisect.bisect_right(letters, target)
+        return letters[index % len(letters)]
