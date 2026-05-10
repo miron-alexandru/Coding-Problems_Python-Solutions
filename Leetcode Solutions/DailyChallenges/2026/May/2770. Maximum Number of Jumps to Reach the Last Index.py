4995cef8021a144ab53623eac409d0a0159aec64
Ake <khadaake@gmail.com>
2026-05-10 18:35:19 +0300

    Solved daily leetcode problem: 2770. Maximum Number of Jumps to Reach the Last Index.

---
+++ Leetcode Solutions/DailyChallenges/2026/May/2770. Maximum Number of Jumps to Reach the Last Index.py
@@ -0,0 +1,62 @@
+# problem link: https://leetcode.com/problems/maximum-number-of-jumps-to-reach-the-last-index/
+
+
+# Description:
+"""
+You are given a 0-indexed array nums of n integers and an integer target.
+
+You are initially positioned at index 0. In one step, you can jump from index i to any index j such that:
+
+0 <= i < j < n
+-target <= nums[j] - nums[i] <= target
+Return the maximum number of jumps you can make to reach index n - 1.
+
+If there is no way to reach index n - 1, return -1.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,6,4,1,2], target = 2
+Output: 3
+Explanation: To go from index 0 to index n - 1 with the maximum number of jumps, you can perform the following jumping sequence:
+- Jump from index 0 to index 1. 
+- Jump from index 1 to index 3.
+- Jump from index 3 to index 5.
+It can be proven that there is no other jumping sequence that goes from 0 to n - 1 with more than 3 jumps. Hence, the answer is 3. 
+Example 2:
+
+Input: nums = [1,3,6,4,1,2], target = 3
+Output: 5
+Explanation: To go from index 0 to index n - 1 with the maximum number of jumps, you can perform the following jumping sequence:
+- Jump from index 0 to index 1.
+- Jump from index 1 to index 2.
+- Jump from index 2 to index 3.
+- Jump from index 3 to index 4.
+- Jump from index 4 to index 5.
+It can be proven that there is no other jumping sequence that goes from 0 to n - 1 with more than 5 jumps. Hence, the answer is 5. 
+Example 3:
+
+Input: nums = [1,3,6,4,1,2], target = 0
+Output: -1
+Explanation: It can be proven that there is no jumping sequence that goes from 0 to n - 1. Hence, the answer is -1. 
+"""
+
+# Solution:
+class Solution:
+    def maximumJumps(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        
+        # dp[i] = maximum jumps needed to reach i
+        dp = [-1] * n
+        dp[0] = 0
+        
+        for i in range(n):
+            if dp[i] == -1:
+                continue
+            
+            for j in range(i + 1, n):
+                if -target <= nums[j] - nums[i] <= target:
+                    dp[j] = max(dp[j], dp[i] + 1)
+        
+        return dp[-1]
