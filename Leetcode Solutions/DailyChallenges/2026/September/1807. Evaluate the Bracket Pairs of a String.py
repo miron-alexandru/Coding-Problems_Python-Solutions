d194a9b874e8fe4c2b502de5e9dc9a97922c93bf
Ake <khadaake@gmail.com>
2026-09-26 09:11:52 +0300

    Solved daily leetcode problem: 1807. Evaluate the Bracket Pairs of a String.

---
+++ Leetcode Solutions/DailyChallenges/2026/September/1807. Evaluate the Bracket Pairs of a String.py
@@ -0,0 +1,62 @@
+# problem link: https://leetcode.com/problems/evaluate-the-bracket-pairs-of-a-string/
+
+
+# Description:
+"""
+You are given a string s that contains some bracket pairs, with each pair containing a non-empty key.
+
+For example, in the string "(name)is(age)yearsold", there are two bracket pairs that contain the keys "name" and "age".
+You know the values of a wide range of keys. This is represented by a 2D string array knowledge where each knowledge[i] = [keyi, valuei] indicates that key keyi has a value of valuei.
+
+You are tasked to evaluate all of the bracket pairs. When you evaluate a bracket pair that contains some key keyi, you will:
+
+Replace keyi and the bracket pair with the key's corresponding valuei.
+If you do not know the value of the key, you will replace keyi and the bracket pair with a question mark "?" (without the quotation marks).
+Each key will appear at most once in your knowledge. There will not be any nested brackets in s.
+
+Return the resulting string after evaluating all of the bracket pairs.
+
+ 
+
+Example 1:
+
+Input: s = "(name)is(age)yearsold", knowledge = [["name","bob"],["age","two"]]
+Output: "bobistwoyearsold"
+Explanation:
+The key "name" has a value of "bob", so replace "(name)" with "bob".
+The key "age" has a value of "two", so replace "(age)" with "two".
+Example 2:
+
+Input: s = "hi(name)", knowledge = [["a","b"]]
+Output: "hi?"
+Explanation: As you do not know the value of the key "name", replace "(name)" with "?".
+Example 3:
+
+Input: s = "(a)(a)(a)aaa", knowledge = [["a","yes"]]
+Output: "yesyesyesaaa"
+Explanation: The same key can appear multiple times.
+The key "a" has a value of "yes", so replace all occurrences of "(a)" with "yes".
+Notice that the "a"s not in a bracket pair are not evaluated.
+"""
+
+
+# Solution:
+
+class Solution:
+    def evaluate(self, s: str, knowledge: list[list[str]]) -> str:
+        values = dict(knowledge)
+        result = []
+        i = 0
+
+        while i < len(s):
+            if s[i] == '(':
+                j = s.index(')', i)
+                key = s[i + 1:j]
+
+                result.append(values.get(key, '?'))
+                i = j + 1
+            else:
+                result.append(s[i])
+                i += 1
+
+        return ''.join(result)
